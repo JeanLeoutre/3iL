f2e01e3f179bfeeb49ebc57578ae62e33e2cfe2c
Jean <jean.leoutre@gmail.com>
2020-03-17 17:47:06 +0100

    Dépôt du code v2

---
+++ TD/src/Test_tri_collection.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test_tri_collection {
+	public static void main(String[] args) {
+		List<Score> listeScore =new LinkedList<Score>();
+		listeScore.add(new Score("Jean",3,65));
+		for(Score s:listeScore) {
+			s.affiche();
+		}
+	}
+}

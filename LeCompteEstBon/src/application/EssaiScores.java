6cc6fe4a629a3b6715d6f60adbff3502da80e7ec
Jean <jean.leoutre@gmail.com>
2020-03-18 10:04:44 +0100

    Merge fichier

--- LeCompteEstBon/src/application/EssaiScores.java
+++ LeCompteEstBon/src/application/EssaiScores.java
@@ -1,3 +1,5 @@
+package application;
+
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,7 +9,7 @@ public class EssaiScores {
 	public static void main(String[] args) throws IOException {
 		List<Score> listeScore=new LinkedList<Score>();
 		GereScores gScore = new GereScores();
-		/**
+
 		gScore.ajouteScore("Marcel", 0, 67);
 		gScore.ajouteScore("Georges",0, 67);
 		gScore.ajouteScore("RENEE", 0, 155);
@@ -19,7 +21,7 @@ public class EssaiScores {
 		gScore.ajouteScore("Georges",92, 84);
 		gScore.ajouteScore("Lucien",10, 220);
 		gScore.ajouteScore("Jean",100, 20);
-		**/
+	
 		gScore.enregistre();
 		gScore.affiche();
 		gScore.export();
--- LeCompteEstBon/src/application/GereScores.java
+++ LeCompteEstBon/src/application/GereScores.java
@@ -1,3 +1,5 @@
+package application;
+
 import java.io.BufferedWriter;
 import java.io.EOFException;
 import java.io.File;
